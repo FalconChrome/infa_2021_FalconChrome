ad1375f003c841d0952da5e4446f189db223c484
FalconChrome <tastylifeeel03@mail.ru>
2021-10-17 22:43:59 +0300

    Started refactoring and mastering Added (not full) documentation to functions Writing function for correct composition of the main picture

--- lab3/16_2.py
+++ lab3/16_2.py
@@ -1,18 +1,18 @@
 import pygame
+from pygame.transform import flip, scale, rotozoom
 from pygame.draw import rect, ellipse
 from pygame.color import Color
 from pygame.rect import Rect
 from pic_params import *
 
 pygame.init()
-
+size = 630, 890
 FPS = 30
 
-size = 630, 890
 bndx, bndy = 0, 0  # boundaries of picture
 
 
-def draw_moved(func, *args, rect, **kwargs):
+def draw_bounded(func, *args, rect, **kwargs):
     '''Function for shift draw, helps make bounds'''
     func(*args, ((rect[0][0] - bndx, rect[0][1] - bndy), rect[1]), **kwargs)
 
@@ -20,14 +20,8 @@ def draw_moved(func, *args, rect, **kwargs):
 # screen size counted with bounds
 screen = pygame.display.set_mode((size[0] - 2 * bndx, size[1] - 2 * bndy))
 
-# initial bg filling
-screen.fill(WHITE)
-draw_moved(rect, screen, Color('azure3'), rect=((0, 0), (630, 565)))
-draw_moved(rect, screen, GROUND_GREEN, rect=((0, 570), (630, 320)))
-draw_moved(ellipse, screen, Color('azure2'), rect=((-50, 740), (800, 300)))
 
-
-def blit_transp(func, screen, color, rect, alpha=100):
+def blit_transp(draw_figure, screen, rect, color=WHITE, alpha=100):
     '''Blits with transparency a figure drawn with a func argument'''
     if type(rect) != Rect:
         rect = Rect(rect)
@@ -36,11 +30,53 @@ def blit_transp(func, screen, color, rect, alpha=100):
     surface = pygame.Surface((4 * bx, 4 * by))  # generating additional surface
     surface.set_colorkey(BLACK)  # making bg unblitable
     surface.set_alpha(alpha)
-    func(surface, color,  ((bx, by), rect.size))
+    draw_figure(surface, color, ((bx, by), rect.size))
     screen.blit(surface, (rect.x - bx, rect.y - by))
 
 
+def master_draw(screen, draw_pic, *args, scale_factor=1, fit_rect=None,
+                orig_size=(630, 890), reverse=False, rotation=False,
+                alpha=255, **kwargs):
+    '''
+    Mastering component to the result screen.
+    With a given fit_rect brings a picture to that's size
+    from original size of drawing.
+    '''
+    if fit_rect is None:
+        fit_rect = Rect(screen)
+    elif type(fit_rect) != Rect:
+        fit_rect = Rect(fit_rect)
+    elif scale_factor == 1:
+        scale_factor = fit_rect.w / orig_size[0]
+    surface = pygame.Surface(orig_size)
+    draw_pic(surface, *args, **kwargs)
+    if rotation:
+        rotozoom(screen, rotation, scale_factor)
+    else:
+        scale(screen, scale_factor)
+    if reverse:
+        flip(surface)
+    blit_transp(lambda *args: None, screen, WHITE, fit_rect, alpha=alpha)
+
+
+def draw_bg(screen):
+    '''
+    Draws a background of the picture.
+    '''
+    screen.fill(WHITE)
+    draw_bounded(rect, screen, Color('azure3'), rect=((0, 0), (630, 565)))
+    draw_bounded(rect, screen, GROUND_GREEN, rect=((0, 570), (630, 320)))
+    draw_bounded(ellipse, screen, Color('azure2'),
+                 rect=((-50, 740), (800, 300)))
+
+
 def ellipse_center(surface, color, coord):
+    '''
+    Draw an ellipse with given center coordinates of that.
+    surface - pygame.Surface object
+    color - pygame.Color object or corresponding tuple
+    coord - coordinate of the center
+    '''
     center, (w, h) = coord
     rect = Rect(0, 0, 2 * w, 2 * h)
     rect.center = center
@@ -49,34 +85,48 @@ def ellipse_center(surface, color, coord):
 
 
 def draw_scrappers(surface):
+    '''
+    Function drawing the scrappers with parameters from data.
+    '''
     for i, (color, scrapper) in enumerate(skyscrappers):
         # print(scrapper)
         w, h = scrapper[1]
         w -= scrapper[0][0]
         h -= scrapper[0][1]
-        draw_moved(rect, surface, Color(color), rect=scrapper)
+        draw_bounded(rect, surface, Color(color), rect=scrapper)
 
 
 def draw_scrappers_orig(surface):
+    '''
+    Function drawing the scrappers with parameters from data.
+    Interfering with spots.
+    '''
     for i, (color, scrapper) in enumerate(skyscrappers):
         # print(scrapper)
         w, h = scrapper[1]
         w -= scrapper[0][0]
         h -= scrapper[0][1]
-        draw_moved(blit_transp, ellipse, surface, Color(PALEGRAY),
-                   rect=spots[i], alpha=140)
-        draw_moved(rect, surface, Color(color), rect=scrapper)
+        draw_bounded(blit_transp, ellipse, surface, color=Color(PALEGRAY),
+                     rect=spots[i], alpha=140)
+        draw_bounded(rect, surface, Color(color), rect=scrapper)
 
 
 def draw_spots(surface, nums=None):
+    '''
+    Function drawing the spots with parameters from data.
+    '''
+
     if nums is None:
         nums = range(7)
     for j in nums:
-        draw_moved(blit_transp, ellipse, surface, Color(PALEGRAY),
-                   rect=spots[j], alpha=140)
+        draw_bounded(blit_transp, ellipse, surface, color=Color(PALEGRAY),
+                     rect=spots[j], alpha=140)
 
 
 def draw_car(surface):
+    '''
+    Function drawing the car with parameters from data.
+    '''
     ellipse_center(screen, BLACK, car['black'][-1])
     for color in car:
         if color != 'black':
@@ -87,10 +137,17 @@ def draw_car(surface):
                 ellipse_center(screen, Color(color), p)
 
 
-draw_scrappers_orig(screen)
-draw_spots(screen, range(4, 7))
-draw_car(screen)
+def draw_result_picture(screen):
+    '''
+    Function drawing the whole picture with parameters from data.
+    '''
+    draw_bg(screen)
+    draw_scrappers_orig(screen)
+    draw_spots(screen, range(4, 7))
+    draw_car(screen)
 
+
+draw_result_picture(screen)
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
